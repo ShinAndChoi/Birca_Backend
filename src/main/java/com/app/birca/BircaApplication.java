c89ab4c2076e9c51d4a952e997a2b4a149a71e83
CMHyuk <alsgur9710@naver.com>
2023-08-17 21:08:34 +0900

    feat : @EnableJpaAuditing, @EnableConfigurationProperties 추가

--- src/main/java/com/app/birca/BircaApplication.java
+++ src/main/java/com/app/birca/BircaApplication.java
@@ -1,9 +1,14 @@
 package com.app.birca;
 
+import com.app.birca.config.JwtConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
+@EnableConfigurationProperties({JwtConfig.class})
 public class BircaApplication {
 
     public static void main(String[] args) {
