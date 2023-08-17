7c198e1139c64c6a7aa7bf1531022316f88a80ce
CMHyuk <alsgur9710@naver.com>
2023-08-17 18:48:03 +0900

    inital commit

---
+++ src/main/java/com/app/birca/BircaApplication.java
@@ -0,0 +1,13 @@
+package com.app.birca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BircaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BircaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/birca/BircaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.birca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BircaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
