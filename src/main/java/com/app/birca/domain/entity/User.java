14b175159c1772189a3320331a0f3a4fa42d0d41
CMHyuk <alsgur9710@naver.com>
2023-08-17 20:47:11 +0900

    feat : User 엔티티

---
+++ src/main/java/com/app/birca/domain/entity/User.java
@@ -0,0 +1,40 @@
+package com.app.birca.domain.entity;
+
+import com.app.birca.domain.BaseEntity;
+import com.app.birca.domain.RoleType;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@Table(name = "users")
+@NoArgsConstructor(access = PROTECTED)
+public class User extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Enumerated(STRING)
+    private RoleType roleType;
+
+    private String nickname;
+    private String email;
+
+    @Builder
+    public User(String nickname, String email) {
+        this.nickname = nickname;
+        this.email = email;
+    }
+
+    public void updateRoleType(String roleType) {
+        this.roleType = RoleType.fromString(roleType);
+    }
+
+}
