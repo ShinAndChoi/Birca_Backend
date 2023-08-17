bc25a4f6b0906eac11654110d436d503d38dffb3
CMHyuk <alsgur9710@naver.com>
2023-08-17 21:05:49 +0900

    feat : BusinessRegistrationError 에러 클래스

---
+++ src/main/java/com/app/birca/exception/BusinessRegistrationError.java
@@ -0,0 +1,16 @@
+package com.app.birca.exception;
+
+public class BusinessRegistrationError extends BircaException {
+
+    private static final String MESSAGE = "국세청에 등록되지 않은 사업자등록번호입니다.";
+
+    public BusinessRegistrationError() {
+        super(MESSAGE);
+    }
+
+    @Override
+    public int getStatusCode() {
+        return 400;
+    }
+
+}
