d1bfccbe1760fc0d3c8ed0b9797fe7c21c90e2c9
CMHyuk <alsgur9710@naver.com>
2023-08-17 20:58:55 +0900

    feat : JwtArgumentResolver - 로그인 중인 회원 id 반환

---
+++ src/main/java/com/app/birca/config/JwtArgumentResolver.java
@@ -0,0 +1,33 @@
+package com.app.birca.config;
+
+import com.app.birca.dto.request.LoginUser;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Slf4j
+public class JwtArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        log.info("supportsParameter 실행");
+        boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
+        boolean hasLoginType = LoginUser.class.isAssignableFrom(parameter.getParameterType());
+        return hasLoginAnnotation && hasLoginType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        log.info("resolveArgument 실행");
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        Long userId = (Long) request.getAttribute("userId");
+
+        log.info("userId = {}", userId);
+        return new LoginUser(userId);
+    }
+
+}
