08b9443df64ebdf7a7b3de251c2e1678621dc49a
CMHyuk <alsgur9710@naver.com>
2023-08-17 20:53:35 +0900

    feat : ReservationRepositoryImpl - 예약 유무 확인

---
+++ src/main/java/com/app/birca/repository/ReservationRepositoryImpl.java
@@ -0,0 +1,27 @@
+package com.app.birca.repository;
+
+import com.app.birca.domain.entity.QReservation;
+import com.app.birca.dto.request.CafeReservationRequest;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ReservationRepositoryImpl implements ReservationCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Boolean isReserved(Long cafeId, CafeReservationRequest request) {
+        QReservation reservation = QReservation.reservation;
+
+        return queryFactory.selectFrom(reservation)
+                .where(
+                        reservation.cafe.id.eq(cafeId),
+                        reservation.startDate.before(request.getEndDate()),
+                        reservation.endDate.after(request.getStartDate()),
+                        reservation.isReserved.isTrue()
+                )
+                .fetch().isEmpty();
+    }
+
+}
