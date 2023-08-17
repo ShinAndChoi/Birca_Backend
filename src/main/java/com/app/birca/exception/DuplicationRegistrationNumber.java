af2d72189e57a955883ff9bffc46129faac4ff89
CMHyuk <alsgur9710@naver.com>
2023-08-17 21:02:07 +0900

    feat : DuplicationRegistrationNumber 에러 클래스

---
+++ src/main/java/com/app/birca/exception/DuplicationRegistrationNumber.java
@@ -0,0 +1,16 @@
+package com.app.birca.exception;
+
+public class DuplicationRegistrationNumber extends BircaException {
+
+    private static final String MESSAGE = "이미 등록된 사업자등록번호입니다.";
+
+    public DuplicationRegistrationNumber() {
+        super(MESSAGE);
+    }
+
+    @Override
+    public int getStatusCode() {
+        return 409;
+    }
+
+}
