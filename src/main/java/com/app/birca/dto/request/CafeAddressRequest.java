8c4beb37d1930b291772abcdf7ed5ab699e3e7ff
CMHyuk <alsgur9710@naver.com>
2023-08-19 23:37:09 +0900

    feat : 카페 주소 dto

---
+++ src/main/java/com/app/birca/dto/request/CafeAddressRequest.java
@@ -0,0 +1,11 @@
+package com.app.birca.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CafeAddressRequest {
+    private String city;
+    private String district;
+    private String area;
+}
