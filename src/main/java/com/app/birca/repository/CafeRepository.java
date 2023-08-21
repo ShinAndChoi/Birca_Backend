5aa1d0717e0cfe0fbc7b5d7c5d263434662203e9
CMHyuk <alsgur9710@naver.com>
2023-08-21 22:11:10 +0900

    feat : 캘린더 날짜 클릭 대관된 카페 조회

--- src/main/java/com/app/birca/repository/CafeRepository.java
+++ src/main/java/com/app/birca/repository/CafeRepository.java
@@ -2,9 +2,16 @@ package com.app.birca.repository;
 
 import com.app.birca.domain.entity.Cafe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface CafeRepository extends JpaRepository<Cafe, Long>, CafeRepositoryCustom {
     List<Cafe> findAll();
+
+    @Query("SELECT DISTINCT c FROM Cafe c JOIN c.reservations r " +
+            "WHERE :selectedDate BETWEEN r.startDate AND r.endDate")
+    List<Cafe> findBySelectedDate(@Param("selectedDate") LocalDate selectedDate);
 }
