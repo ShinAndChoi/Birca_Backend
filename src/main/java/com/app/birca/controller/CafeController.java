a444d79008a5a72de290d26bcafe1b3ce83e181c
CMHyuk <alsgur9710@naver.com>
2023-08-23 00:06:17 +0900

    feat : 캘린더 날짜 클릭 카페 조회 API

--- src/main/java/com/app/birca/controller/CafeController.java
+++ src/main/java/com/app/birca/controller/CafeController.java
@@ -10,10 +10,14 @@ import com.app.birca.dto.response.CafeSearchResponse;
 import com.app.birca.service.CafeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class CafeController {
@@ -21,23 +25,41 @@ public class CafeController {
     private final CafeService cafeService;
 
     @PostMapping("/cafe/register")
-    public Long saveCafe(@Login LoginUser loginUser, @ModelAttribute SaveCafeRequest request) {
+    public Long saveCafe(@Login LoginUser loginUser, @ModelAttribute SaveCafeRequest request) throws IOException {
+        log.info("cafeName = {} ", request.getCafeName());
+        log.info("introduction = {} ", request.getIntroduction());
+        log.info("file = {}", request.getFile().getOriginalFilename());
+        log.info("city = {}", request.getAddress().getCity());
+        log.info("district = {}", request.getAddress().getDistrict());
+        log.info("area = {}", request.getAddress().getArea());
         return cafeService.saveCafe(loginUser, request);
     }
 
     @PatchMapping("/cafe/{cafeId}/update")
     public void updateCafe(@PathVariable Long cafeId, @ModelAttribute UpdateCafeRequest request) {
+        log.info("cafeName = {} ", request.getCafeName());
+        log.info("introduction = {} ", request.getIntroduction());
+        log.info("file = {}", request.getFile().getOriginalFilename());
         cafeService.updateCafe(cafeId, request);
     }
 
     @GetMapping("/cafes")
     public List<CafeSearchResponse> searchCafe(@RequestParam int page, @RequestBody @Valid CafeSearchRequest request) {
+        log.info("idol = {}", request.getIdolName());
+        log.info("startDate = {} ", request.getStartDate());
+        log.info("endDate = {} ", request.getEndDate());
         return cafeService.searchCafe(page, request);
     }
 
     @GetMapping("/cafe/{cafeId}/details")
     public CafeResponse getCafeDetails(@PathVariable Long cafeId) {
         return cafeService.getCafeDetails(cafeId);
+    }
+
+    @GetMapping("/cafes/reserved-on")
+    public List<CafeSearchResponse> findBySelectedDate(@RequestParam LocalDate selectedDate) {
+        log.info("selectedDate = {} ", selectedDate);
+        return cafeService.findBySelectedDate(selectedDate);
     }
 
 }
