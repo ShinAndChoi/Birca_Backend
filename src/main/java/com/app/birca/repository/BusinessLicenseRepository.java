002615ce02ec7d1b2f8b3518d6f98ecb28e99986
CMHyuk <alsgur9710@naver.com>
2023-08-23 15:49:36 +0900

    feat : 사업자등록증 처리 유무 조회 기능

--- src/main/java/com/app/birca/repository/BusinessLicenseRepository.java
+++ src/main/java/com/app/birca/repository/BusinessLicenseRepository.java
@@ -1,8 +1,15 @@
 package com.app.birca.repository;
 
 import com.app.birca.domain.entity.BusinessLicense;
+import com.app.birca.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BusinessLicenseRepository extends JpaRepository<BusinessLicense, Long> {
+
     Boolean existsByRegistrationNumber(String registrationNumber);
+
+    @Query(value = "SELECT b.isRegisteredBusiness FROM BusinessLicense b WHERE b.user =:user")
+    Boolean existsByUser(User user);
+
 }
