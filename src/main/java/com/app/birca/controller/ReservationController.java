e354d547329a609105c5825c422d32e4b80f4d51
CMHyuk <alsgur9710@naver.com>
2023-08-23 15:51:06 +0900

    feat : 예약 취소 API

--- src/main/java/com/app/birca/controller/ReservationController.java
+++ src/main/java/com/app/birca/controller/ReservationController.java
@@ -5,11 +5,10 @@ import com.app.birca.dto.request.CafeReservationRequest;
 import com.app.birca.dto.request.LoginUser;
 import com.app.birca.service.ReservationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class ReservationController {
@@ -18,7 +17,14 @@ public class ReservationController {
 
     @PostMapping("/{cafeId}/reservation")
     public void reservation(@Login LoginUser loginUser, @PathVariable Long cafeId, @RequestBody CafeReservationRequest request) {
-        reservationService.reservation(loginUser, cafeId, request);
+        log.info("idolName = {} ", request.getIdolName());
+        log.info("startDate = {}, endDate = {} ", request.getStartDate(), request.getEndDate());
+        reservationService.reserveCafe(loginUser, cafeId, request);
+    }
+
+    @DeleteMapping("/{reservationId}/cancelReservation")
+    public void cancelCafeReservation(@Login LoginUser loginUser, @PathVariable Long reservationId) {
+        reservationService.cancelCafeReservation(reservationId, loginUser);
     }
 
 }
