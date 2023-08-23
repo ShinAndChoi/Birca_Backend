be3f8cfaecbaf58bca91bf468d7bd4cbe53be315
CMHyuk <alsgur9710@naver.com>
2023-08-23 15:50:08 +0900

    feat : 사업자등록증 처리 에러 클래스

---
+++ src/main/java/com/app/birca/exception/BusinessRegistrationPendingException.java
@@ -0,0 +1,16 @@
+package com.app.birca.exception;
+
+public class BusinessRegistrationPendingException extends BircaException {
+
+    private static final String MESSAGE = "사업자등록증 심사가 통과되지 않았습니다.";
+
+    public BusinessRegistrationPendingException() {
+        super(MESSAGE);
+    }
+
+    @Override
+    public int getStatusCode() {
+        return 422;
+    }
+
+}
