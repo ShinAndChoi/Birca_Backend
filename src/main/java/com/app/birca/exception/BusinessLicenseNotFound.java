a8af2d531e0644f07e8291e5f6fa75e027c3598d
CMHyuk <alsgur9710@naver.com>
2023-08-23 15:48:44 +0900

    feat : BusinessLicenseNotFound 클래스

---
+++ src/main/java/com/app/birca/exception/BusinessLicenseNotFound.java
@@ -0,0 +1,16 @@
+package com.app.birca.exception;
+
+public class BusinessLicenseNotFound extends BircaException {
+
+    private static final String MESSAGE = "사업자등록증이 존재하지 않습니다.";
+
+    public BusinessLicenseNotFound() {
+        super(MESSAGE);
+    }
+
+    @Override
+    public int getStatusCode() {
+        return 404;
+    }
+
+}
