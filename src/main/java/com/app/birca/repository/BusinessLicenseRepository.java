90f16186c73dd25edf51bcbf1d4a18e3ec313782
CMHyuk <alsgur9710@naver.com>
2023-08-25 14:03:36 +0900

    refactor : 사용하지 않는 메서드 제거

--- src/main/java/com/app/birca/repository/BusinessLicenseRepository.java
+++ src/main/java/com/app/birca/repository/BusinessLicenseRepository.java
@@ -6,10 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface BusinessLicenseRepository extends JpaRepository<BusinessLicense, Long> {
-
     Boolean existsByRegistrationNumber(String registrationNumber);
-
-    @Query(value = "SELECT b.isRegisteredBusiness FROM BusinessLicense b WHERE b.user =:user")
-    Boolean existsByUser(User user);
-
 }
