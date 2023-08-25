0711193efe51203b8caed7a2f63e6131e0d1dbe8
CMHyuk <alsgur9710@naver.com>
2023-08-25 13:51:42 +0900

    chore : deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 REPOSITORY=/home/ec2-user/action
-PROJECT_NAME=birca-0.0.1-SNAPSHOT
+PROJECT_NAME=birca-0.0.1-SNAPSHOT.jar
 
 cd $REPOSITORY/$PROJECT_NAME/
 
@@ -25,22 +25,18 @@ cp $REPOSITORY/$PROJECT_NAME.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.jar)
+CURRENT_PID=$(pgrep -f birca-0.0.1-SNAPSHOT.jar)
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/action/deploy.log
 else
    echo "> kill -15 $CURRENT_PID"
    kill -15 $CURRENT_PID
    sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 애플리케이션 배포" >> /home/ec2-user/action/deploy.log
 
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep ${PROJECT_NAME}.jar | tail -n 1)
-
-echo "> JAR Name: $JAR_NAME"
-
-nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
+nohup java -jar $REPOSITORY/birca-0.0.1-SNAPSHOT.jar 2>&1 &
