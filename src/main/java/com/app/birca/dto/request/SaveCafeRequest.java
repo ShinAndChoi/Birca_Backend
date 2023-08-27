b5508d299959336f73c21520f61a482643a4af0a
CMHyuk <alsgur9710@naver.com>
2023-08-27 21:33:38 +0900

    refactor : @Setter -> 생성자로 변경

--- src/main/java/com/app/birca/dto/request/SaveCafeRequest.java
+++ src/main/java/com/app/birca/dto/request/SaveCafeRequest.java
@@ -1,11 +1,13 @@
 package com.app.birca.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
-@Setter
+@AllArgsConstructor
 public class SaveCafeRequest {
     private String cafeName;
     private String introduction;
--- src/main/java/com/app/birca/dto/request/UpdateCafeRequest.java
+++ src/main/java/com/app/birca/dto/request/UpdateCafeRequest.java
@@ -1,11 +1,12 @@
 package com.app.birca.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
-@Setter
+@AllArgsConstructor
 public class UpdateCafeRequest {
     private String cafeName;
     private String introduction;
