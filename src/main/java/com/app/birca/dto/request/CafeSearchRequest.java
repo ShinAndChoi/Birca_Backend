14baaf31c993d60b1cdecd6aac44a19db452e684
CMHyuk <alsgur9710@naver.com>
2023-08-31 19:05:11 +0900

    refactor : @AllArgsConstructor 추가

--- src/main/java/com/app/birca/dto/request/CafeSearchRequest.java
+++ src/main/java/com/app/birca/dto/request/CafeSearchRequest.java
@@ -1,11 +1,13 @@
 package com.app.birca.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDate;
 
 @Getter
+@AllArgsConstructor
 public class CafeSearchRequest {
     @NotBlank(message = "아이돌 입력은 필수사항입니다.")
     private String idolName;
