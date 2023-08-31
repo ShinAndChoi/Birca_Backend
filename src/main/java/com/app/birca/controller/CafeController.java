23ada3c53557dcca992fb4e2ef5df6909de550f7
CMHyuk <alsgur9710@naver.com>
2023-08-31 19:05:55 +0900

    refactor : 검색 로직 분리로 인한 코드 제거

--- src/main/java/com/app/birca/controller/CafeController.java
+++ src/main/java/com/app/birca/controller/CafeController.java
@@ -1,14 +1,12 @@
 package com.app.birca.controller;
 
 import com.app.birca.config.Login;
-import com.app.birca.dto.request.CafeSearchRequest;
 import com.app.birca.dto.request.LoginUser;
 import com.app.birca.dto.request.SaveCafeRequest;
 import com.app.birca.dto.request.UpdateCafeRequest;
 import com.app.birca.dto.response.CafeResponse;
 import com.app.birca.dto.response.CafeSearchResponse;
 import com.app.birca.service.CafeService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -39,14 +37,6 @@ public class CafeController {
         log.info("introduction = {} ", request.getIntroduction());
         log.info("file = {}", request.getFile().getOriginalFilename());
         cafeService.updateCafe(cafeId, request);
-    }
-
-    @GetMapping("/cafes")
-    public List<CafeSearchResponse> searchCafe(@RequestParam int page, @RequestBody @Valid CafeSearchRequest request) {
-        log.info("idol = {}", request.getIdolName());
-        log.info("startDate = {} ", request.getStartDate());
-        log.info("endDate = {} ", request.getEndDate());
-        return cafeService.searchCafe(page, request);
     }
 
     @GetMapping("/cafe/{cafeId}/details")
