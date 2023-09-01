c5843b46676d6cf5c968ec932177cffeb461db7c
CMHyuk <alsgur9710@naver.com>
2023-09-01 21:15:28 +0900

    feat : CafeImageRepository

---
+++ src/main/java/com/app/birca/repository/CafeImageRepository.java
@@ -0,0 +1,7 @@
+package com.app.birca.repository;
+
+import com.app.birca.domain.entity.CafeImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CafeImageRepository extends JpaRepository<CafeImage, Long> {
+}
