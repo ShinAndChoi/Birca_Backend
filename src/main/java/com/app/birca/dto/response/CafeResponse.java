feb73bfa01998e6b426a9c06ca050cd354db1e19
CMHyuk <alsgur9710@naver.com>
2023-09-01 21:15:11 +0900

    refactor : 카페 검색 결과 반환 데이터 변경 - 이미지 하나 -> 여러 개

--- src/main/java/com/app/birca/dto/response/CafeResponse.java
+++ src/main/java/com/app/birca/dto/response/CafeResponse.java
@@ -1,7 +1,11 @@
 package com.app.birca.dto.response;
 
+import com.app.birca.domain.entity.CafeImage;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class CafeResponse {
@@ -9,14 +13,15 @@ public class CafeResponse {
     private final Long cafeId;
     private final String cafeName;
     private final String introduction;
-    private final String imageUrl;
+    private final List<String> imageUrl;
 
     @Builder
-    public CafeResponse(Long cafeId, String cafeName, String introduction, String imageUrl) {
+    public CafeResponse(Long cafeId, String cafeName, String introduction, List<CafeImage> cafeImages) {
         this.cafeId = cafeId;
         this.cafeName = cafeName;
         this.introduction = introduction;
-        this.imageUrl = imageUrl;
+        this.imageUrl = cafeImages.stream().map(i -> i.getImageUrl())
+                .collect(Collectors.toList());
     }
 
 }
