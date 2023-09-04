d03ac3243afa352684e5db85224aef836d3c95e2
CMHyuk <alsgur9710@naver.com>
2023-09-04 10:54:32 +0900

    refactor : MultipartFile dto로 이동

--- src/main/java/com/app/birca/dto/request/SaveCafeRequest.java
+++ src/main/java/com/app/birca/dto/request/SaveCafeRequest.java
@@ -2,6 +2,9 @@ package com.app.birca.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -10,4 +13,6 @@ public class SaveCafeRequest {
     private String introduction;
     private String address;
     private String contact;
+    private MultipartFile businessLicense;
+    private List<MultipartFile> cafeImages;
 }
