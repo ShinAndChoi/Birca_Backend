9a1d43bceeb0c6edeb5e234e627f499bc20136ad
CMHyuk <alsgur9710@naver.com>
2023-09-06 20:08:46 +0900

    feat : 대관 리스트 조회 - userId

--- src/main/java/com/app/birca/repository/ReservationRepository.java
+++ src/main/java/com/app/birca/repository/ReservationRepository.java
@@ -3,8 +3,10 @@ package com.app.birca.repository;
 import com.app.birca.domain.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long>, ReservationCustom {
     Optional<Reservation> findByIdAndUserId(Long reservationId, Long userId);
+    List<Reservation> findByUserId(Long userId);
 }
