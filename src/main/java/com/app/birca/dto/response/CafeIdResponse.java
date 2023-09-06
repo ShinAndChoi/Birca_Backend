a2b38740d876cb318f646ce06fe8446331fa499f
CMHyuk <alsgur9710@naver.com>
2023-09-06 20:32:02 +0900

    feat : cafeId dto

---
+++ src/main/java/com/app/birca/dto/response/CafeIdResponse.java
@@ -0,0 +1,10 @@
+package com.app.birca.dto.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CafeIdResponse {
+    private final Long cafeId;
+}
