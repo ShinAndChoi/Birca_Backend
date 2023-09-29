eaabc7a3eac5709441c6f1c13ac83a21f30d86e0
CMHyuk <alsgur9710@naver.com>
2023-09-29 20:35:15 +0900

    feat : Idol 엔티티

---
+++ src/main/java/com/app/birca/domain/entity/Idol.java
@@ -0,0 +1,35 @@
+package com.app.birca.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Idol {
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "idol_id")
+    private Long id;
+
+    private String idolName;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Idol(String idolName, User user) {
+        this.idolName = idolName;
+        this.user = user;
+    }
+
+    public void updateIdolName(String idolName) {
+        this.idolName = idolName;
+    }
+
+}
