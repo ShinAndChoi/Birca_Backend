5667cd7ca47cc63c7b750908d17d2642ead79cf8
CMHyuk <alsgur9710@naver.com>
2023-10-05 20:43:16 +0900

    feat : FavoriteIdolRepository

---
+++ src/main/java/com/app/birca/repository/IdolGroupRepository.java
@@ -0,0 +1,7 @@
+package com.app.birca.repository;
+
+import com.app.birca.domain.entity.IdolGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IdolGroupRepository extends JpaRepository<IdolGroup, Long> {
+}
