d3122be8aa731af38feef7315fafe850a81f827e
CMHyuk <alsgur9710@naver.com>
2023-10-08 15:55:54 +0900

    feat : 아이돌 이름 저장 dto

---
+++ src/main/java/com/app/birca/dto/request/IdolNameRequest.java
@@ -0,0 +1,10 @@
+package com.app.birca.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class IdolNameRequest {
+    private String koreanName;
+    private String englishName;
+}
