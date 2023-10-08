a53e1599a01a35d988842cc123ea43121adc0286
CMHyuk <alsgur9710@naver.com>
2023-10-08 15:53:10 +0900

    feat : 아이돌 그룹명 조회 기능

--- src/main/java/com/app/birca/repository/IdolGroupRepository.java
+++ src/main/java/com/app/birca/repository/IdolGroupRepository.java
@@ -3,5 +3,8 @@ package com.app.birca.repository;
 import com.app.birca.domain.entity.IdolGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IdolGroupRepository extends JpaRepository<IdolGroup, Long> {
+    Optional<IdolGroup> findByKoreanName(String koreanName);
 }
