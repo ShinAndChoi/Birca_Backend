2e18ec2913de1477f971dbad2c985916b878ce0c
CMHyuk <alsgur9710@naver.com>
2023-10-08 15:55:41 +0900

    feat : 아이돌 그룹 id로 아이돌 조회

--- src/main/java/com/app/birca/repository/IdolRepository.java
+++ src/main/java/com/app/birca/repository/IdolRepository.java
@@ -3,5 +3,8 @@ package com.app.birca.repository;
 import com.app.birca.domain.entity.Idol;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IdolRepository extends JpaRepository<Idol, Long> {
+    List<Idol> findByIdolGroupId(Long idolGroupId);
 }
