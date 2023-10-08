99fc8381e0b42a2604ffc483ea1d914b2d95dc8f
CMHyuk <alsgur9710@naver.com>
2023-10-08 15:54:44 +0900

    feat : 아이돌 그룹 저장 dto

---
+++ src/main/java/com/app/birca/dto/request/InsertIdolGroupRequest.java
@@ -0,0 +1,13 @@
+package com.app.birca.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+public class InsertIdolGroupRequest {
+    private String koreanName;
+    private String englishName;
+    private MultipartFile multipartFile;
+}
