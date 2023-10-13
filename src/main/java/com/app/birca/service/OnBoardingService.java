5b69980b9c738a8c0ce932d2e263a0bed277589b
CMHyuk <alsgur9710@naver.com>
2023-10-13 10:55:58 +0900

    feat : 카페 정보 등록

--- src/main/java/com/app/birca/service/OnBoardingService.java
+++ src/main/java/com/app/birca/service/OnBoardingService.java
@@ -1,16 +1,27 @@
 package com.app.birca.service;
 
 import com.app.birca.domain.Category;
+import com.app.birca.domain.RoleType;
+import com.app.birca.domain.entity.Cafe;
 import com.app.birca.domain.entity.Idol;
 import com.app.birca.domain.entity.IdolGroup;
+import com.app.birca.domain.entity.User;
+import com.app.birca.dto.request.LoginUser;
+import com.app.birca.dto.request.SaveCafeInformRequest;
 import com.app.birca.dto.response.IdolGroupResponse;
 import com.app.birca.dto.response.IdolResponse;
+import com.app.birca.dto.response.businesslicense.BusinessLicenseResponse;
+import com.app.birca.exception.UserNotFound;
+import com.app.birca.repository.CafeRepository;
 import com.app.birca.repository.IdolGroupRepository;
 import com.app.birca.repository.IdolRepository;
+import com.app.birca.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 import static java.util.stream.Collectors.*;
@@ -20,8 +31,12 @@ import static java.util.stream.Collectors.*;
 @Transactional(readOnly = true)
 public class OnBoardingService {
 
+    private final UserRepository userRepository;
+    private final CafeRepository cafeRepository;
     private final IdolRepository idolRepository;
     private final IdolGroupRepository idolGroupRepository;
+    private final BusinessLicenseService businessLicenseService;
+    private final OcrService ocrService;
 
     public List<IdolGroupResponse> findAllIdolGroups() {
         List<IdolGroup> groups = idolGroupRepository.findAll();
@@ -68,4 +83,32 @@ public class OnBoardingService {
                 .collect(toList());
     }
 
+    @Transactional
+    public void saveCafeInform(LoginUser loginUser, SaveCafeInformRequest request) throws IOException {
+        User user = userRepository.findById(loginUser.getId())
+                .orElseThrow(UserNotFound::new);
+
+//        RoleType roleType = user.getRoleType();
+//        if (!roleType.getType().equals("cafeOwner")) {
+//            throw new IllegalArgumentException("카페 사장만 등록할 수 있습니다.");
+//        }
+
+        String ownerName = request.getOwnerName();
+        String cafeName = request.getCafeName();
+        String contactNumber = request.getContactNumber();
+        MultipartFile businessLicense = request.getBusinessLicense();
+
+//        businessLicenseService.uploadBusinessLicense(businessLicense);
+//        BusinessLicenseResponse businessLicenseInfo = ocrService.getBusinessLicenseInfo(businessLicense, "사업자 등록증");
+//        businessLicenseService.saveRegistrationNumber(loginUser, businessLicenseInfo);
+
+        Cafe cafe = Cafe.builder()
+                .ownerName(ownerName)
+                .cafeName(cafeName)
+                .contact(contactNumber)
+                .user(user)
+                .build();
+
+        cafeRepository.save(cafe);
+    }
 }
