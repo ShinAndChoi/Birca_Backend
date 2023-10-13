542e7424ecda24459b961f75ce7f58405904c71f
CMHyuk <alsgur9710@naver.com>
2023-10-13 10:56:09 +0900

    feat : 카페 정보 등록 API

--- src/main/java/com/app/birca/controller/OnBoardingController.java
+++ src/main/java/com/app/birca/controller/OnBoardingController.java
@@ -1,15 +1,19 @@
 package com.app.birca.controller;
 
+import com.app.birca.config.Login;
+import com.app.birca.dto.request.LoginUser;
+import com.app.birca.dto.request.SaveCafeInformRequest;
 import com.app.birca.dto.response.IdolGroupResponse;
 import com.app.birca.dto.response.IdolResponse;
 import com.app.birca.service.OnBoardingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class OnBoardingController {
@@ -34,6 +38,12 @@ public class OnBoardingController {
     @GetMapping("/idols")
     public List<IdolResponse> getIdols(@RequestParam String groupName) {
         return onBoardingService.getIdols(groupName);
+    }
+
+    @PostMapping("/save/cafeInform")
+    public void saveCafeInform(@Login LoginUser loginUser, @ModelAttribute SaveCafeInformRequest request) throws IOException {
+        log.info("businessLicense file Name = {}", request.getBusinessLicense().getOriginalFilename());
+        onBoardingService.saveCafeInform(loginUser, request);
     }
 
 }
