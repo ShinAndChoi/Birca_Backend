a85403c93cd2a499e2dcf663b3f2009ac0601442
CMHyuk <alsgur9710@naver.com>
2023-10-29 10:35:35 +0900

    refactor : 카페 저장 dto 데이터 추가

--- src/main/java/com/app/birca/dto/request/SaveCafeRequest.java
+++ src/main/java/com/app/birca/dto/request/SaveCafeRequest.java
@@ -4,15 +4,32 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class SaveCafeRequest {
+
     private String cafeName;
+    private String ownerName;
     private String introduction;
-    private String address;
     private String contact;
-    private List<MultipartFile> businessLicense;
+    private int capacity;
+    private int price;
+    private Double xAxis;
+    private Double yAxis;
+    private String area;
+    private List<LocalDate> localDates;
+    private MultipartFile businessLicense;
     private List<MultipartFile> cafeImages;
+
+    public Double getxAxis() {
+        return xAxis;
+    }
+
+    public Double getyAxis() {
+        return yAxis;
+    }
+
 }
