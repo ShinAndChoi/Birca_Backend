34eac55ff3d82b5fd09383e887b99f54cf9bdb63
CMHyuk <alsgur9710@naver.com>
2023-10-29 10:34:53 +0900

    refactor : 카페 저장 로직 변경

--- src/main/java/com/app/birca/controller/CafeController.java
+++ src/main/java/com/app/birca/controller/CafeController.java
@@ -3,11 +3,9 @@ package com.app.birca.controller;
 import com.app.birca.config.Login;
 import com.app.birca.dto.request.LoginUser;
 import com.app.birca.dto.request.SaveCafeRequest;
-import com.app.birca.dto.request.SaveCafeRequestV2;
 import com.app.birca.dto.request.UpdateCafeRequest;
 import com.app.birca.dto.response.CafeIdResponse;
 import com.app.birca.dto.response.CafeResponse;
-import com.app.birca.dto.response.CafeSearchResponse;
 import com.app.birca.service.BusinessLicenseService;
 import com.app.birca.service.CafeService;
 import com.app.birca.service.OcrService;
@@ -17,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.List;
 
 @Slf4j
@@ -33,28 +30,14 @@ public class CafeController {
     public CafeIdResponse saveCafe(@Login LoginUser loginUser, @ModelAttribute SaveCafeRequest request) throws IOException {
         log.info("cafeName = {} ", request.getCafeName());
         log.info("introduction = {} ", request.getIntroduction());
-        log.info("businessLicense = {}", request.getBusinessLicense().get(0).getOriginalFilename());
         log.info("cafeImages = {}", request.getCafeImages().get(0).getOriginalFilename());
-        log.info("address = {}", request.getAddress());
         log.info("contact = {}", request.getContact());
 
         //businessLicenseService.uploadBusinessLicense(businessLicense);
         //BusinessLicenseResponse businessLicenseInfo = ocrService.getBusinessLicenseInfo(businessLicense, "사업자 등록증");
         //businessLicenseService.saveRegistrationNumber(loginUser, businessLicenseInfo);
 
-        Long cafeId = cafeService.saveCafe(loginUser, request, request.getCafeImages());
-        return new CafeIdResponse(cafeId);
-    }
-
-    @PostMapping("/cafe/register/v2")
-    public CafeIdResponse saveCafe(@Login LoginUser loginUser, @RequestPart SaveCafeRequestV2 request,
-                         @RequestPart MultipartFile businessLicense, @RequestPart List<MultipartFile> cafeImages) throws IOException {
-        log.info("cafeName = {} ", request.getCafeName());
-        log.info("introduction = {} ", request.getIntroduction());
-        log.info("businessLicense = {}", businessLicense.getOriginalFilename());
-        log.info("area = {}", request.getAddress());
-
-        Long cafeId = cafeService.saveCafeV2(loginUser, request, cafeImages);
+        Long cafeId = cafeService.saveCafe(loginUser, request);
         return new CafeIdResponse(cafeId);
     }
 
@@ -69,12 +52,6 @@ public class CafeController {
     @GetMapping("/cafe/{cafeId}/details")
     public CafeResponse getCafeDetails(@PathVariable Long cafeId) {
         return cafeService.getCafeDetails(cafeId);
-    }
-
-    @GetMapping("/cafes/reserved-on")
-    public List<CafeSearchResponse> findBySelectedDate(@RequestParam LocalDate selectedDate) {
-        log.info("selectedDate = {} ", selectedDate);
-        return cafeService.findBySelectedDate(selectedDate);
     }
 
 }
--- src/main/java/com/app/birca/service/CafeService.java
+++ src/main/java/com/app/birca/service/CafeService.java
@@ -1,18 +1,15 @@
 package com.app.birca.service;
 
-import com.app.birca.domain.entity.Cafe;
-import com.app.birca.domain.entity.CafeImage;
-import com.app.birca.domain.entity.User;
+import com.app.birca.domain.entity.*;
 import com.app.birca.dto.request.LoginUser;
 import com.app.birca.dto.request.SaveCafeRequest;
-import com.app.birca.dto.request.SaveCafeRequestV2;
 import com.app.birca.dto.request.UpdateCafeRequest;
 import com.app.birca.dto.response.CafeResponse;
-import com.app.birca.dto.response.CafeSearchResponse;
 import com.app.birca.exception.CafeNotFound;
 import com.app.birca.exception.UserNotFound;
 import com.app.birca.repository.CafeImageRepository;
 import com.app.birca.repository.CafeRepository;
+import com.app.birca.repository.ReservationAvailabilityRepository;
 import com.app.birca.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,64 +20,64 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 
-import static java.util.stream.Collectors.toList;
-
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class CafeService {
 
+    private final BusinessLicenseService businessLicenseService;
+    private final ReservationAvailabilityRepository reservationAvailabilityRepository;
     private final CafeImageRepository cafeImageRepository;
     private final UserRepository userRepository;
     private final CafeRepository cafeRepository;
     private final S3Service s3Service;
 
     @Transactional
-    public Long saveCafe(LoginUser loginUser, SaveCafeRequest request, List<MultipartFile> cafeImages) throws IOException {
+    public Long saveCafe(LoginUser loginUser, SaveCafeRequest request) throws IOException {
         User user = userRepository.findById(loginUser.getId())
                 .orElseThrow(UserNotFound::new);
 
         Cafe cafe = Cafe.builder()
                 .cafeName(request.getCafeName())
+                .ownerName(request.getOwnerName())
                 .introduction(request.getIntroduction())
-                .address(request.getAddress())
+                .xAxis(request.getxAxis())
+                .yAxis(request.getyAxis())
+                .area(request.getArea())
+                .capacity(request.getCapacity())
+                .price(request.getPrice())
                 .contact(request.getContact())
                 .user(user)
                 .build();
 
         cafeRepository.save(cafe);
 
-        for (MultipartFile image : cafeImages) {
-            String imageUrl = s3Service.uploadImage(image);
-            CafeImage cafeImage = new CafeImage(imageUrl, cafe);
-            cafeImageRepository.save(cafeImage);
-        }
+        MultipartFile businessLicense = request.getBusinessLicense();
+//        businessLicenseService.uploadBusinessLicense(businessLicense);
+//        BusinessLicenseResponse businessLicenseInfo = ocrService.getBusinessLicenseInfo(businessLicense, "사업자 등록증");
+//        businessLicenseService.saveRegistrationNumber(loginUser, businessLicenseInfo);
+
+        saveCafeImages(request, cafe);
+        saveReservationAvailability(request, cafe);
 
         return cafe.getId();
     }
 
-    @Transactional
-    public Long saveCafeV2(LoginUser loginUser, SaveCafeRequestV2 request, List<MultipartFile> cafeImages) throws IOException {
-        User user = userRepository.findById(loginUser.getId())
-                .orElseThrow(UserNotFound::new);
+    private void saveReservationAvailability(SaveCafeRequest request, Cafe cafe) {
+        List<LocalDate> localDates = request.getLocalDates();
+        for (LocalDate localDate : localDates) {
+            ReservationAvailability reservationAvailability = new ReservationAvailability(localDate, cafe);
+            reservationAvailabilityRepository.save(reservationAvailability);
+        }
+    }
 
-        Cafe cafe = Cafe.builder()
-                .cafeName(request.getCafeName())
-                .introduction(request.getIntroduction())
-                .address(request.getAddress())
-                .contact(request.getContact())
-                .user(user)
-                .build();
-
-        cafeRepository.save(cafe);
-
+    private void saveCafeImages(SaveCafeRequest request, Cafe cafe) throws IOException {
+        List<MultipartFile> cafeImages = request.getCafeImages();
         for (MultipartFile image : cafeImages) {
             String imageUrl = s3Service.uploadImage(image);
             CafeImage cafeImage = new CafeImage(imageUrl, cafe);
             cafeImageRepository.save(cafeImage);
         }
-
-        return cafe.getId();
     }
 
     //카페 수정
@@ -102,9 +99,6 @@ public class CafeService {
             cafeImageRepository.save(newCafeImage);
             cafe.getCafeImages().add(newCafeImage);
         }
-
-        cafe.updateCafe(request.getCafeName(), request.getIntroduction(),
-                request.getAddress(), request.getContact(), user);
     }
 
     //카페 상세 페이지
@@ -118,16 +112,6 @@ public class CafeService {
                 .introduction(cafe.getIntroduction())
                 .cafeImages(cafe.getCafeImages())
                 .build();
-    }
-
-    //캘린더에서 클릭한 날짜에 대한 카페 검색
-    public List<CafeSearchResponse> findBySelectedDate(LocalDate selectedDate) {
-        List<Cafe> cafes = cafeRepository.findBySelectedDate(selectedDate);
-        return cafes.stream().map(c -> CafeSearchResponse.builder()
-                        .cafeName(c.getCafeName())
-                        .address(c.getAddress())
-                        .build())
-                .collect(toList());
     }
 
 }
